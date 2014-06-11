990009389598f2a77cc18951319f6bf1aee27f9b
Fish Yu <yhl10000@gmail.com>
2014-06-11 23:27:32 +0800

    this is the initial version

---
+++ EverythingForFish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EverythingForFish
+//
+//  Created by Fish Yu on 14-6-11.
+//  Copyright (c) 2014年 yhl10000@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
