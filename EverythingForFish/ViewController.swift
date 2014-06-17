19a9ff5a501f086f645c4fc8e43b27c68c6071a4
Fish Yu <yhl10000@gmail.com>
2014-06-17 23:19:59 +0800

    add navigation view controller

--- EverythingForFish/ViewController.swift
+++ EverythingForFish/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +23,22 @@ class ViewController: UIViewController {
     }
 
 
+
+    func collectionView(collectionView: UICollectionView!, didSelectItemAtIndexPath indexPath: NSIndexPath!){
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int{
+        return 10;
+    }
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell!
+    {
+        var cell : UICollectionViewCell  =  collectionView.dequeueReusableCellWithReuseIdentifier("only_lable", forIndexPath:indexPath) as UICollectionViewCell;
+        var l100 : UILabel =  cell.viewWithTag(100) as UILabel;
+        l100.text = "测试";
+        return cell;
+    }
 }
 
