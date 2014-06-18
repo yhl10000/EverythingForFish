bcf3cae3f4d666cf50197fd751ec60663ae944fd
Fish Yu <yhl10000@gmail.com>
2014-06-18 23:10:56 +0800

    replaced the collection view with tableview,

---
+++ EverythingForFish/fish.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+    println("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    println("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    println("(\(x), \(y)) is just some arbitrary point")
+}
